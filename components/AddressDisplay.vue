83bdddaa9549306ada7ee913352be66e6e96b892
ZOU <253058016@qq.com>
2025-05-18 23:03:15 +0800

    update

--- components/AddressDisplay.vue
+++ components/AddressDisplay.vue
@@ -3,7 +3,7 @@
     <div class="flex items-center gap-2 bg-accented rounded-full px-4 py-2 cursor-pointer hover:bg-accented/80 transition-colors"
         @click="copyAddress">
         <img :src="imageUrl" v-if="imageUrl" class="w-7 h-7 rounded-full border-2 border-green-400" alt="avatar" />
-        <Identicon :value="address" :size="28" theme="polkadot" v-else />
+        <Avatar :name="address || zeroAddress" :size="28" variant="beam" v-else />
         <span class="font-medium" v-if="address">{{ formatAddress(address) }}</span>
         <UIcon name="ci:copy" size="24" class="cursor-pointer hover:text-primary-500 text-sm" />
     </div>
@@ -12,7 +12,8 @@
 <script setup lang="ts">
 // @ts-nocheck
 import { formatAddress } from '~/utils/display'
-import Identicon from '@polkadot/vue-identicon'
+import Avatar from "vue-boring-avatars"
+import { zeroAddress } from 'viem'
 
 const props = defineProps<{
     imageUrl?: string
