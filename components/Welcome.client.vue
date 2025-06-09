b04492911e3f9c2fd1f13aba5b3519084db969f4
ZOU <253058016@qq.com>
2025-06-09 11:01:08 +0800

    更改app 标题和 ico

--- components/Welcome.client.vue
+++ components/Welcome.client.vue
@@ -1,7 +1,6 @@
 <template>
   <div class="flex flex-col container-size h-[100vh] rounded-xl bg-[var(--ui-bg)] shadow-lg p-4 banner justify-center items-center">
-    <h1 class="text-4xl font-bold mt-10 mb-1">Semi</h1>
-    <div class="text-lg font-medium mb-4">你的智能数字货币钱包</div>
+    <h1 class="font-medium mb-4 text-2xl">南塘数字身份</h1>
     <UButton v-if="ready" color="primary" size="xl" class="flex justify-center items-center w-60" to="/login">开始使用</UButton>
     <UProgress class="mt-10" v-else />
   </div>
