a9c62ae445b75bdcbb841bade46cf6bba2535a01
ZOU <253058016@qq.com>
2025-06-09 13:53:48 +0800

    更改logo

--- components/Welcome.client.vue
+++ components/Welcome.client.vue
@@ -1,6 +1,7 @@
 <template>
-  <div class="flex flex-col container-size h-[100vh] rounded-xl bg-[var(--ui-bg)] shadow-lg p-4 banner justify-center items-center">
-    <h1 class="font-medium mb-4 text-2xl">南塘数字身份</h1>
+  <div class="flex flex-col container-size h-[100vh] rounded-xl bg-[var(--ui-bg)] shadow-lg p-4 justify-center items-center">
+    <div class="logo" />
+    <h1 class="font-medium mb-4 text-xl mt-4 title">南塘数字身份</h1>
     <UButton v-if="ready" color="primary" size="xl" class="flex justify-center items-center w-60" to="/login">开始使用</UButton>
     <UProgress class="mt-10" v-else />
   </div>
@@ -17,10 +18,22 @@ defineProps({
 </script>
 
 <style scoped>
-    .banner {
-        background-image: url('/images/home_banner.png');
-        background-size: 120% auto;
-        background-position: center -10px;
-        background-repeat: no-repeat;
+.logo {
+      width: 280px;
+      height: 237px;
+      background-image: url('/images/nantang_logo.png');
+      background-size: 100% 100%;
+      background-position: center;
+      background-repeat: no-repeat;
+    }
+   
+    .dark .logo {
+      background-image: url('/images/nantang_logo_dark.png');
+    }
+
+    .title {
+      width: 210px;
+      text-align: justify;
+      text-align-last: justify;
     }
 </style>
